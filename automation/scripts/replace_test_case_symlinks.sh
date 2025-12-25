aa4ad5aed44314d68d39244f85c4332e0e99c682
actyp <actypaldos@gmail.com>
2025-12-25 05:34:39 -0500

    fix: improve and move replace_test_case_symlinks to scripts

--- automation/scripts/replace_test_case_symlinks.sh
+++ automation/scripts/replace_test_case_symlinks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 root_dir="$(realpath "${1}")"
+if [[ ! -d "${root_dir}" ]]; then
+    echo "Provided directory not found: ${root_dir}"
+    exit 0
+fi
 
 find "${root_dir}/Public-Tests/P01_sphincs_plus" -type l -iname '*test_case' -print0 |
     while IFS= read -r -d $'\0' slink; do
